4651d9b22fdf149bd8bb49fe7245b09e727301c7
Sanad <155049358+SanadSonone@users.noreply.github.com>
2024-12-25 07:16:05 +0000

    basics of javascript

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 766629
+let accountEmail = "sanad@google.com"
+var accountPassword = "12345"
+accountCity = "amravati"
+let accountState;
+
+
+accountEmail = "sonone@gmail.com"
+accountPassword = "485474"
+accountCity = "daryapur"
+
+console.log(accountId);
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
